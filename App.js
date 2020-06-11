2300037ce147f55e635bd1ca824a43fcaff8732c
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-06-11 16:20:49 -0300

    Pure React | Moving to App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+const Wanted = () => {
+  return React.createElement("div", { class: "poster" }, [
+    React.createElement("img", { src: "./assets/gato.jpg", alt: "Gato" }),
+    React.createElement("h2", {}, "Gato Bege"),
+    React.createElement(
+      "h3",
+      {},
+      "Possui um colarinho azul e tem um leve machucado nas costas. Atende pelo nome de Lucy."
+    ),
+    React.createElement("div", { class: "contact" }, [
+      React.createElement("p", {}, "14 99862 5506"),
+      React.createElement("p", {}, "gmantiqueira@gmail.com"),
+    ]),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { class: "container" }, [
+    React.createElement(Wanted),
+    React.createElement(Wanted),
+    React.createElement(Wanted),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
