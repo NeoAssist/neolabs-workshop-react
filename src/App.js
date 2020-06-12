806715d656896b1d58c0582e30e76c3c619eeb7e
Gabriel Antiqueira <gantiqueira@neoassist.com>
2020-06-12 14:39:04 -0300

    Converting to JSX, Add React ESLint config

--- src/App.js
+++ src/App.js
@@ -8,33 +8,34 @@ import Dog from "../assets/dog.jpg";
 import Parrot from "../assets/bird.jpg";
 
 const App = () => {
-  return React.createElement("div", { class: "container" }, [
-    React.createElement(Wanted, {
-      image: Cat,
-      title: "Gato bege",
-      owner: "Gabriel Antiqueira",
-      description:
-        "Possui um colarinho azul e tem um leve machucado nas costas. Atende pelo nome de Lucy.",
-      tel: "14 99862 5506",
-      email: "gmantiqueira@gmail.com",
-    }),
-    React.createElement(Wanted, {
-      image: Dog,
-      title: "Cachorro sorridente",
-      owner: "Fulano da Silva",
-      description: "Nós vivemos numa sociedade em que.",
-      tel: "14 99862 5506",
-      email: "fulano@gmail.com",
-    }),
-    React.createElement(Wanted, {
-      image: Parrot,
-      title: "Papagaio maromba",
-      owner: "Léo Stronda",
-      description: "Ama batata doce e frango.",
-      tel: "14 99862 5506",
-      email: "stronda@gmail.com",
-    }),
-  ]);
+  return (
+    <div className="container">
+      <Wanted
+        image={Cat}
+        title="Gato bege"
+        owner="Gabriel Antiqueira"
+        description="Possui um colarinho azul e tem um leve machucado nas costas. Atende pelo nome de Lucy."
+        tel="14 99862 5506"
+        email="gmantiqueira@gmail.com"
+      />
+      <Wanted
+        image={Dog}
+        title="Cachorro sorridente"
+        owner="Fulano da Silva"
+        description="Nós vivemos numa sociedade em que."
+        tel="14 99862 5506"
+        email="fulano@gmail.com"
+      />
+      <Wanted
+        image={Parrot}
+        title="Papagaio maromba"
+        owner="Léo Stronda"
+        description="Ama batata doce e frango."
+        tel="14 99862 5506"
+        email="stronda@gmail.com"
+      />
+    </div>
+  )
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/Wanted/index.js
+++ src/components/Wanted/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Wanted = (props) => {
-  return React.createElement("div", { class: "poster" }, [
-    React.createElement("img", { src: props.image, alt: props.title }),
-    React.createElement("h2", {}, props.title),
-    React.createElement("h3", {}, props.description),
-    React.createElement("div", { class: "contact" }, [
-      React.createElement("p", {}, props.tel),
-      React.createElement("p", {}, props.email),
-    ]),
-  ]);
+  return (
+    <div className="poster">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h3>{props.description}</h3>
+      <div className="contact">
+        <p>{props.tel}</p>
+        <p>{props.email}</p>
+      </div>
+    </div>
+  )
 };
 
 export default Wanted;
